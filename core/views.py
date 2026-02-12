fba84a093d3fa517a1d17abd57aef65da511ecd1
Kaitlin Fong <krfong@ualberta.ca>
2026-02-11 17:42:54 -0700

    Fix gitignore placement

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Social Distribution Project: base setup ✅")
+    return HttpResponse("Slay nodes project woot ✅")
 
