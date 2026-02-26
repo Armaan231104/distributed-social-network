ba21039fed67cb9e06a4c427b56ee4e69945cde4
Nikhil Joshi <nikhiljsh2004@gmail.com>
2026-02-26 16:33:24 -0700

    fixed minor import errors

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views, stream
+from . import views
 
 urlpatterns = [
     path("api/entries/create/", views.create_entry),
     path("api/entries/mine/", views.my_entries),
     path("api/entries/<uuid:entry_id>/edit/", views.edit_entry),
     path("api/entries/<uuid:entry_id>/delete/", views.delete_entry),
-    path('stream/', stream, name='stream')
+    path('stream/', views.stream, name='stream')
 ]
