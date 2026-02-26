997d14c038f061fe52e99cadf8df03c66c510b89
Armaan231104 <aikhan@ualberta.ca>
2026-02-25 17:04:42 -0700

    Entry, ownership and soft delete (3 user stories)

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Entry(models.Model):
+    VISIBILITY_CHOICES = [
+        ("PUBLIC", "Public"),
+        ("UNLISTED", "Unlisted"),
+        ("FRIENDS", "Friends"),
+        ("DELETED", "Deleted"),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    visibility = models.CharField(max_length=10, choices=VISIBILITY_CHOICES, default="PUBLIC")
+
+    def soft_delete(self):
+        self.visibility = "DELETED"
+        self.save()
