41ba78c29913c1f5d5de3ed3c79c2ec3e54d6269
swcarson <swcarson@ualberta.ca>
2026-03-22 17:36:45 -0600

    fix

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-only-change-me")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",") + [
-    "https://cmput404-crimson-swcarson-55711a031595.herokuapp.com/",
+    "cmput404-crimson-swcarson-55711a031595.herokuapp.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = ['https://cmput404-crimson-swcarson-55711a031595.herokuapp.com']
