f888dee73cd885e2674f90acc722776b5ea3b0f1
Nikhil Joshi <nikhiljsh2004@gmail.com>
2026-03-23 22:46:06 -0600

    Fix Cloudinary storage

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -169,9 +169,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 if not DEBUG:
-    CLOUDINARY_STORAGE = {
-        'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-        'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-        'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-    }
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
